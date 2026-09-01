10ac0e2334556731e05daceacf8a4014ad4fc010
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-01 15:15:23 +0500

    Custom Decorators

--- src/Guards/authguard.ts
+++ src/Guards/authguard.ts
@@ -1,6 +1,6 @@
-import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { Observable } from "rxjs";
-
+@Injectable()
 export class AuthGuard implements CanActivate{
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const req=context.switchToHttp().getRequest();
