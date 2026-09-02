8cae61c4c4ad736f17a52b030d91a28d2012ffc2
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-02 22:22:26 +0500

    Http Methods

--- src/Products/product.controller.ts
+++ src/Products/product.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
-
+import { Body, Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
+let products=["biscuit","watch","cake","macroni"];
 @Controller('products')
 export class ProductsController{
     
@@ -7,4 +7,30 @@ export class ProductsController{
     get(){
         return "Hello World";
     }
+    @Get('/featured')
+    getProducts(){
+        return products;
+    }
+    @Get(':id')
+    getId(){
+        return `id is here`;
+    }
+    @Post()
+    insertProducts(@Body() product:string){
+        products.push(product);
+    }
+    @Put()
+    updateProducts(@Body()newproducts:string[]){
+        products=newproducts;
+    }
+    @Patch()
+    replaceProduct(@Body()product:string){
+        products.pop();
+        products.push(product);
+    }
+    @Delete()
+    deleteProduct(){
+        products.pop();
+    }
+
 }
