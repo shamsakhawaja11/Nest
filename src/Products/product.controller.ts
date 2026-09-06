4fe53470b72152393e2fcaafec0b2d547ba7594c
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-06 10:05:47 +0500

    rest convention and nouns not verbs rule

--- src/Products/product.controller.ts
+++ src/Products/product.controller.ts
@@ -1,13 +1,47 @@
-import { Body, Controller, Delete, Get, Head, Header, Headers, HttpCode, Param, Patch, Post, Put, Query, Res } from "@nestjs/common";
-let products=["biscuit","watch","cake","macroni"];
+import { BadRequestException, Body, Controller, Delete, Get, Head, Header, Headers, HttpCode, NotFoundException, Param, Patch, Post, Put, Query, Res } from "@nestjs/common";
+
+let products={
+    prod1: { id:8,name:'biscuit'},
+    prod2: { id:2,name:'birryani'}
+};
+
 import type{ Response } from "express";
 @Controller('products')
 export class ProductsController {
     
-    @Get()
-    getme(){
-        return "Hello World";
+    @Get(':id')
+    getId(@Param('id')id:string){
+        for(let item in products){
+            if(products[item].id==Number(id)){
+                return "id Found";
+            }
+        }
+        throw new NotFoundException(`${id} not found`)
+
     }
+    @Post()
+    insertProduct(@Body()product:any){
+        if(product['name']==undefined||product['name']==""){
+            throw new BadRequestException(`${name} is invalid`);
+        }
+    }
+    @Delete(':id')
+    deleteProduct(@Param('id')id:string){
+        for(let item in products){
+            if(products[item].id===Number(id)){
+                products[item]=null;
+                return `product with ${id} removed`;
+            }
+        }
+        throw new NotFoundException(`${id}not found`);
+    }
+
+
+
+    // @Get()
+    // getme(){
+    //     return "Hello World";
+    // }
     // @Get('/featured')
     // getProducts(){
     //     return products;
@@ -40,22 +74,22 @@ export class ProductsController {
     //}
 
 
-    @Get('/debug/headers')
-    getHeaders(@Headers('useragent')useragent:string) {
-        return {userAgent:useragent};
-    }
-    @HttpCode(201)
-    @Post()
-    insert(@Body()product:string) {
-        products.push(product);
-    }
-    @Header('X-powered-by','Nest Learning')
-    @Get()
-    get(@Res()res:any) {
-        return products;
-    }
-    @Get('/res-test')
-    resTest(@Res() res: Response) {
-     res.status(201).json(products); // does the client actually get this?
-    }
+    // @Get('/debug/headers')
+    // getHeaders(@Headers('useragent')useragent:string) {
+    //     return {userAgent:useragent};
+    // }
+    // @HttpCode(201)
+    // @Post()
+    // insert(@Body()product:string) {
+    //     products.push(product);
+    // }
+    // @Header('X-powered-by','Nest Learning')
+    // @Get()
+    // get(@Res()res:any) {
+    //     return products;
+    // }
+    // @Get('/res-test')
+    // resTest(@Res() res: Response) {
+    //  res.status(201).json(products); // does the client actually get this?
+    // }
 }
