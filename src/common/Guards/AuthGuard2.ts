2eb08565db6a2a2ea9e24a59a8f4284f19dbbdd2
Shamsa Khawaja <shamsakhawaja11@gmail.com>
2026-09-17 22:10:51 +0500

    update guards

--- src/common/Guards/AuthGuard2.ts
+++ src/common/Guards/AuthGuard2.ts
@@ -2,17 +2,21 @@ import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from
 import { Observable } from "rxjs";
 
 @Injectable()
-export class AuthGuard implements CanActivate{
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const req=context.switchToHttp().getRequest()
-        if(req['authorization'].bearer===null){
-            throw new UnauthorizedException('not allowed')
-        }else if(req['authorization'].bearer==='valid'){
-            return {id:1,name:'shamsa'
-            }
-        }else{
-            throw new UnauthorizedException('not allowed')
+export class AuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const req = context.switchToHttp().getRequest();
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            throw new UnauthorizedException('not allowed');
         }
+        const token = authHeader.split(' ')[1];
+
+        if (token !== 'valid') {
+            throw new UnauthorizedException('not allowed');
+        }
+        req.user = { id: 1, name: 'shamsa' };
+        return true;
     }
 }
 
